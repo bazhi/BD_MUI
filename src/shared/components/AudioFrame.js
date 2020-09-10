fcb880de491304411a7357456e381127927b92c6
bazhicc <bazhicc@163.com>
2020-09-10 17:17:02 +0800

    修改

--- src/shared/components/AudioFrame.js
+++ src/shared/components/AudioFrame.js
@@ -4,46 +4,48 @@ import MusicNoteIcon from '@material-ui/icons/MusicNote';
 import MusicOffIcon from '@material-ui/icons/MusicOff';
 import IconButton from "@material-ui/core/IconButton";
 
-function AudioFrame(props)
-{
+function AudioFrame(props) {
 	const {src} = props;
-	const [playing, setPlaying] = useState(0);
+	const [playing, setPlaying] = useState(false);
 	const bgMusic = useRef();
 	
-	const OnAction = useCallback(() =>{
+	const OnAction = useCallback(() => {
 		if (bgMusic.current) {
 			if (bgMusic.current.paused) {
 				bgMusic.current.play();
-			}else{
+			} else {
 				bgMusic.current.pause();
 			}
 		}
-		if(bgMusic.current.paused){
-			setPlaying(1);
-		}else{
-			setPlaying(2);
-		}
-
-	}, [setPlaying, bgMusic]);
-	
-	useEffect(()=>{
-		bgMusic.current.pause();
 	}, [bgMusic]);
 	
-	return(
+	const OnMount = useCallback(() => {
+		if (bgMusic.current) {
+			bgMusic.current.addEventListener("play", function () {
+				setPlaying(true);
+			});
+			bgMusic.current.addEventListener("pause", function () {
+				setPlaying(false);
+			});
+		}
+	}, [bgMusic, setPlaying]);
+	
+	useEffect(OnMount);
+	
+	return (
 		<Fragment>
 			<audio autoPlay={"autoplay"} loop={"loop"} preload={"auto"} ref={bgMusic}
 			       src={src}>
 				你的浏览器不支持audio标签
 			</audio>
 			{
-				playing > 1 && <IconButton onClick={OnAction}>
-					<MusicNoteIcon/>
+				playing && <IconButton onClick={OnAction}>
+					<MusicNoteIcon />
 				</IconButton>
 			}
 			{
-				playing <= 1 && <IconButton onClick={OnAction}>
-					<MusicOffIcon/>
+				!playing && <IconButton onClick={OnAction}>
+					<MusicOffIcon />
 				</IconButton>
 			}
 		</Fragment>
