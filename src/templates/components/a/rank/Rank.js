60492cb375a8244206b071ebe4f1ec427b101637
bazhicc <bazhicc@163.com>
2020-09-18 14:12:03 +0800

    修改

--- src/templates/components/a/rank/Rank.js
+++ src/templates/components/a/rank/Rank.js
@@ -11,6 +11,14 @@ import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import AxiosCache from "shared/components/AxiosCache";
 import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import Badge from "@material-ui/core/Badge";
+
+const StyledBadge = withStyles((theme) => ({
+	badge: {
+
+	},
+}))(Badge);
 
 const styles = (theme) => ({
 	wrapper: {
@@ -58,7 +66,13 @@ function Rank(props) {
 			url: `/data/${id}/rank.json`,
 			method: 'get'
 		}).then(function (res) {
-			setUserData(res.data);
+			let data = res.data;
+			if(data.list){
+				data.list.sort(function (a, b){
+					return (b.vote + b.favour * data.favourScore) - (a.vote + a.favour * data.favourScore);
+				});
+			}
+			setUserData(data);
 		}).catch(function (error) {
 			console.log(error);
 		});
@@ -68,37 +82,51 @@ function Rank(props) {
 		LoadData(actionID);
 	}, [LoadData, actionID]);
 	
+	const GetRankColor = useCallback((index)=>{
+		if(index === 0){
+			return "primary";
+		}else if(index === 1){
+			return "secondary"
+		}else if(index === 2){
+			return "error";
+		}
+		return "default";
+	}, []);
+	
 	return (
 		<Fragment>
 			{userData && (
 				<div className={classNames("lg-p-top", classes.wrapper)}>
 					<div className={classNames("container-fluid", classes.container)}>
-						<Typography variant="h3" align="center" style={{color:userTheme.default.color}}>
+						<Typography variant="h3" align="center" style={{color: userTheme.default.color}}>
 							{userData.title}
 						</Typography>
 						<Box display="flex" justifyContent="center" className="column">
 							<TableContainer component={Paper} className={classes.table}>
-								<Table aria-label="customized table" align={"center"}>
+								<Table aria-label="customized table" align={"center"}  >
 									<TableHead>
 										<TableRow>
-											<StyledTableCell>名称</StyledTableCell>
-											<StyledTableCell align="right">排名</StyledTableCell>
-											<StyledTableCell align="right">简介</StyledTableCell>
-											<StyledTableCell align="right">赞</StyledTableCell>
-											<StyledTableCell align="right">票数</StyledTableCell>
-										
+											<StyledTableCell align="center">排名</StyledTableCell>
+											<StyledTableCell align="center">名称</StyledTableCell>
+											<StyledTableCell align="center">编号</StyledTableCell>
+											<StyledTableCell align="center">赞</StyledTableCell>
+											<StyledTableCell align="center">票数</StyledTableCell>
+											<StyledTableCell align="center">总计</StyledTableCell>
 										</TableRow>
 									</TableHead>
 									<TableBody>
-										{userData.list && userData.list.map((row) => (
-											<StyledTableRow key={row.name}>
-												<StyledTableCell component="th" scope="row">
-													{row.name}
+										{userData.list && userData.list.map((row, index) => (
+											<StyledTableRow key={index}>
+												<StyledTableCell align="center" component="th" scope="row">
+													<StyledBadge badgeContent={index+1} color={GetRankColor(index)} anchorOrigin={{vertical: 'top', horizontal: 'left'}}>
+														<Avatar alt="Remy Sharp" src={row.image} />
+													</StyledBadge>
 												</StyledTableCell>
-												<StyledTableCell align="right">{row.brief}</StyledTableCell>
-												<StyledTableCell align="right">{row.fat}</StyledTableCell>
-												<StyledTableCell align="right">{row.carbs}</StyledTableCell>
-												<StyledTableCell align="right">{row.protein}</StyledTableCell>
+												<StyledTableCell align="center">{row.name}</StyledTableCell>
+												<StyledTableCell align="center">{row.id}</StyledTableCell>
+												<StyledTableCell align="center">{row.favour}</StyledTableCell>
+												<StyledTableCell align="center">{row.vote}</StyledTableCell>
+												<StyledTableCell align="center">{row.vote + row.favour * userData.favourScore}</StyledTableCell>
 											</StyledTableRow>
 										))}
 									</TableBody>
